e6b5073a0f3a90dac24cba3ddef12947033d31a4
Jinseo Sung <jinseosung15@gmail.com>
2022-03-17 22:23:23 +0100

    Created and deleting To Dos

---
+++ js/todo.js
@@ -0,0 +1,29 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.querySelector("#todo-list");
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const btn = document.createElement("button");
+  btn.innerText = "❌";
+  btn.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(btn);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
